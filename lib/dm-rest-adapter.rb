853fff80a81915a3632f67795183f0ca06e65e5f
Chris Corbyn <chris@w3style.co.uk>
2011-06-13 22:50:42 +1000

    First (rough) phase in refactor to replace Net::HTTP with RestClient

--- lib/dm-rest-adapter.rb
+++ lib/dm-rest-adapter.rb
@@ -2,14 +2,12 @@ require 'rexml/document'
 
 require 'rest-client'
 
-require 'cgi'  # for CGI.escape
 require 'addressable/uri'
 
 require 'dm-core'
 require 'dm-serializer'
 
 require 'dm-rest-adapter/adapter'
-require 'dm-rest-adapter/connection'
 require 'dm-rest-adapter/formats'
 require 'dm-rest-adapter/format/xml'
 require 'dm-rest-adapter/format/json'
