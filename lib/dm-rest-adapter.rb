49564d9da801f5c9a3802dcdd22b2d3ec0d31688
Chris Corbyn <chris@w3style.co.uk>
2011-06-14 00:52:47 +1000

    Rename formats.rb => format.rb

--- lib/dm-rest-adapter.rb
+++ lib/dm-rest-adapter.rb
@@ -8,7 +8,7 @@ require 'dm-core'
 require 'dm-serializer'
 
 require 'dm-rest-adapter/adapter'
-require 'dm-rest-adapter/formats'
+require 'dm-rest-adapter/format'
 require 'dm-rest-adapter/format/xml'
 require 'dm-rest-adapter/format/json'
 require 'dm-rest-adapter/exceptions'
