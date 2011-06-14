70b9c89307596d6bbdc086fe8459595548854f3c
Chris Corbyn <chris@w3style.co.uk>
2011-06-14 14:04:33 +1000

    Specs on the JSON parsing... still needs hooking up with the adapter

--- lib/dm-rest-adapter/format/json.rb
+++ lib/dm-rest-adapter/format/json.rb
@@ -4,6 +4,49 @@ module DataMapperRest
       def default_options
         { :mime => "application/json", :extension => "json" }
       end
+      
+      def string_representation(resource)
+        resource.to_json
+      end
+      
+      def update_attributes(resource, body)
+        return if DataMapper::Ext.blank?(body)
+
+        model      = resource.model
+        properties = model.properties(repository_name)
+
+        parse_record(body, model).each do |key, value|
+          if property = properties[key.to_sym]
+            property.set!(resource, value)
+          end
+        end
+      end
+      
+      def parse_record(json, model)
+        hash = JSON.parse(json)
+        field_to_property = Hash[ model.properties(repository_name).map { |p| [ p.field, p ] } ]
+        record_from_hash(hash, field_to_property)
+      end
+      
+      def parse_collection(json, model)
+        array = JSON.parse(json)
+        field_to_property = Hash[ model.properties(repository_name).map { |p| [ p.field, p ] } ]
+        array.collect do |hash|
+          record_from_hash(hash, field_to_property)
+        end
+      end
+      
+      private
+      
+      def record_from_hash(hash, field_to_property)
+        record = {}
+        hash.each_pair do |field, value|
+          next unless property = field_to_property[field]
+          record[field] = property.typecast(value)
+        end
+        
+        record
+      end
     end
   end
 end
--- spec/semipublic/format/json_spec.rb
+++ spec/semipublic/format/json_spec.rb
@@ -6,6 +6,11 @@ describe DataMapperRest::Format::Json do
     
     its(:mime) { should == "application/json" }
     its(:extension) { should == "json" }
+    
+    it "appends the default extension to the resource path" do
+      format = DataMapperRest::Format::Json.new
+      format.resource_path(Book.new.model).should == "books.json"
+    end
   end
 
   describe "custom extension" do
@@ -13,6 +18,11 @@ describe DataMapperRest::Format::Json do
     
     its(:mime) { should == "application/json" }
     its(:extension) { should == "yml" }
+    
+    it "appends the extension to the resource path" do
+      format = DataMapperRest::Format::Json.new(:extension => "data")
+      format.resource_path(Book.new.model).should == "books.data"
+    end
   end
 
   describe "no extension" do
@@ -20,6 +30,11 @@ describe DataMapperRest::Format::Json do
     
     its(:mime) { should == "application/json" }
     its(:extension) { should be_nil }
+    
+    it "does not append an extension to the resource path" do
+      format = DataMapperRest::Format::Json.new(:extension => nil)
+      format.resource_path(Book.new.model).should == "books"
+    end
   end
 
   describe "empty extension" do
@@ -27,5 +42,85 @@ describe DataMapperRest::Format::Json do
     
     its(:mime) { should == "application/json" }
     its(:extension) { should be_nil }
+    
+    it "does not append an extension to the resource path" do
+      format = DataMapperRest::Format::Json.new(:extension => nil)
+      format.resource_path(Book.new.model).should == "books"
+    end
+  end
+  
+  describe "#string_representation" do
+    before(:each) do
+      @format = DataMapperRest::Format::Json.new
+      @time = DateTime.now
+      @json = '{"id":1,"created_at":"' + @time.to_s + '","title":"Testing","author":"Testy McTesty"}'
+    end
+
+    it "returns a JSON string representing the resource" do
+      book = Book.new(
+        :id         => 1,
+        :created_at => @time,
+        :title      => "Testing",
+        :author     => "Testy McTesty"
+      )
+      book_json = @format.string_representation(book)
+      book_json.should == @json
+    end
+  end
+  
+  describe "#update_attributes" do
+    before(:each) do
+      @format = DataMapperRest::Format::Json.new
+      @time = DateTime.new
+      @json = '{"id":1,"created_at":"' + @time.to_s + '","title":"Testing","author":"Testy McTesty"}'
+    end
+    
+    it "updates the attributes in the resource based on the response" do
+      book = Book.new
+      @format.update_attributes(book, @json)
+      
+      book.id.should == 1
+      book.created_at.should == @time
+      book.title.should == "Testing"
+      book.author.should == "Testy McTesty"
+    end
+  end
+  
+  describe "#parse_record" do
+    before(:each) do
+      @format = DataMapperRest::Format::Json.new
+      @time = DateTime.new
+      @json = '{"id":1,"created_at":"' + @time.to_s + '","title":"Testing","author":"Testy McTesty"}'
+    end
+    
+    it "loads a record from the string representation" do
+      record = @format.parse_record(@json, Book)
+      record["id"].should == 1
+      record["created_at"].should == @time
+      record["title"].should == "Testing"
+      record["author"].should == "Testy McTesty"
+    end
+  end
+  
+  describe "#parse_collection" do
+    before(:each) do
+      @format = DataMapperRest::Format::Json.new
+      @time = DateTime.new
+      @json = '[{"id":1,"created_at":"' + @time.to_s + '","title":"Testing","author":"Testy McTesty"},' +
+        '{"id":2,"created_at":"' + @time.to_s + '","title":"Testing 2","author":"Besty McBesty"}]'
+    end
+    
+    it "loads a recordset from the string representation" do
+      collection = @format.parse_collection(@json, Book)
+      collection.should have(2).entries
+      collection[0]["id"].should == 1
+      collection[0]["created_at"].should == @time
+      collection[0]["title"].should == "Testing"
+      collection[0]["author"].should == "Testy McTesty"
+      collection[1]["id"].should == 2
+      collection[1]["created_at"].should == @time
+      collection[1]["title"].should == "Testing 2"
+      collection[1]["author"].should == "Besty McBesty"
+    end
   end
 end
